e5fdcb3e8891d819a1b80ef13d8fd4521739fd64
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-05 19:47:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function destructivelyPrependDriver (name) {
 function destructivelyRemoveLastDriver () {
   drivers.pop();
 }
-function destructivelyRemoveLastDriver () {
+function destructivelyRemoveFirstDriver () {
   drivers.shift();
 }
